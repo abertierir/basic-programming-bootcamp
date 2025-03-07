50f71890f15ddeaad4413c24375bffa0e93d111f
eliana <elianasipa@hotmail.es>
2025-03-07 05:40:12 -0500

    factura

---
+++ eliana siza/02_factura/factura.py
@@ -0,0 +1,9 @@
+valor_base=100
+iva=0.19
+valor_total=(valor_base*iva)+(valor_base)
+print("FACTURA DE VENTA")
+print("VALOR BASE:"+str(valor_base))
+print("IVA:"+str(iva))
+print("VALOR TOTAL:"+str(valor_total))
+
+
