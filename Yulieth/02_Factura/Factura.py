d2107384a69f7d95a48d4dc2d46fa08f6a82c193
yuliethmguevara1 <yuliethmguevara@gmail.com>
2025-03-07 05:39:47 -0500

    Commit Factura

---
+++ Yulieth/02_Factura/Factura.py
@@ -0,0 +1,14 @@
+valor_base = 100
+iva = 0.21
+total = valor_base * (1+iva)
+
+print("La factura tiene un valor de total: "+str(total)+". Con una base de: "+str(valor_base)+" Y un iva de "+ str(iva))
+
+
+       
+
+
+
+
+
+
