b32f48f789971588875a2980f3996a23893a552d
yuliethmguevara1 <yuliethmguevara@gmail.com>
2025-03-11 05:59:30 -0500

    commit1

---
+++ Yulieth/03_input/Hypotenusa.py
@@ -0,0 +1,5 @@
+Lado_a= input("Dime cuanto mide el primer lado ")
+Lado_b= input("Dime cuando mide el segundo lado ")
+Resultado= (int(Lado_a)**2)+(int(Lado_b)**2)
+Hypotenusa = (Resultado)**0.5
+print("La Hypotenusa del triángulo es igual a: ", Hypotenusa)
